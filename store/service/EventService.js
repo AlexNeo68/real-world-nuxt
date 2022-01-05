0cf10110b79c01860967aa31aa339de6f660d5b1
Александр Неудахин <aleksandrneudahin@Aleksandrs-Air.localnet>
2022-01-05 08:04:33 +0300

    prepare to deployment

--- store/service/EventService.js
+++ store/service/EventService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-    baseURL: 'http://127.0.0.1:3000',
+    baseURL: 'https://my-json-server.typicode.com/AlexNeo68/real-world-nuxt',
     withCredentials: false,
     headers: {
         Accept: 'application/json',
